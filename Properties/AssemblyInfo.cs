1a957755370052256059a09b78b52b7c8d24a196
Justin Walgran <jwalgran@azavea.com>
2013-09-19 12:47:27 -0400

    1.1.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b7f5a41f-054f-4a64-80f6-5098e01e50c0")]
 
-[assembly: AssemblyVersion("1.0.4.0")]
-[assembly: AssemblyFileVersion("1.0.4.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 // Used for nuget packaging
-[assembly: AssemblyInformationalVersion("1.0.4")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
